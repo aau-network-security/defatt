9543112cea3d9d6b579e65c018ebbd405de53297
Ahmet Turkmen <f.ahmet.turkmen@icloud.com>
2020-09-28 13:57:36 +0200

    remove unused function from ovsmanagement
    Signed-off-by: Ahmet Turkmen <f.ahmet.turkmen@icloud.com>

--- controller/ovs.go
+++ controller/ovs.go
@@ -18,12 +18,3 @@ func (c *OvsManagement) CreateBridge(bridgeName string) error {
 	}
 	return nil
 }
-
-func (c *OvsManagement) addPort(bridgeName, port string) error {
-
-	if err := c.VSwitch.AddPort(bridgeName, port); err != nil {
-		log.Error().Msgf("Error on adding port on openvswitch %v", err)
-		return err
-	}
-	return nil
-}
