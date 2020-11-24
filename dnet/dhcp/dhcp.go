1de4df71434962f5ef05fb06ebf9f6b4ba8e407c
Ahmet Turkmen <f.ahmet.turkmen@icloud.com>
2020-11-24 13:12:07 +0100

    modify dhcp
    Signed-off-by: Ahmet Turkmen <f.ahmet.turkmen@icloud.com>

--- dnet/dhcp/dhcp.go
+++ dnet/dhcp/dhcp.go
@@ -34,6 +34,12 @@ type Server struct {
 	ipList   map[string]string
 }
 
+type LanSpec struct {
+	NetI   string
+	LANTag string
+	Bridge string
+}
+
 func createDHCPFile(nets Networks) string {
 	var tpl bytes.Buffer
 	tmpl := template.Must(template.ParseFiles("/home/ubuntu/defat/dnet/dhcp/dhcpd.conf.tmpl"))
@@ -59,22 +65,20 @@ func addToSwitch(c *controller.NetController, net Subnet, bridge, cid string) er
 }
 
 //New creates a DHCP server which will be listening on the interfaces given as the argument
-func New(ctx context.Context, ifaces map[string]string, bridge string, c *controller.NetController) (*Server, error) {
+func New(ctx context.Context, l *LanSpec, c *controller.NetController) (*Server, error) {
 	ipList := make(map[string]string)
 	var networks Networks
 	ipPool := controller.NewIPPoolFromHost()
-	for vl, vt := range ifaces {
-		var sNet Subnet
-		randIP, _ := ipPool.Get()
-		sNet.Interface = vl
-		sNet.Vlan = vt
-		sNet.Network = randIP + ".0"
-		sNet.Min = randIP + ".6"
-		sNet.Max = randIP + ".254"
-		sNet.Router = randIP + ".1"
-		networks.Subnets = append(networks.Subnets, sNet)
-		ipList[sNet.Vlan] = randIP
-	}
+	var sNet Subnet
+	randIP, _ := ipPool.Get()
+	sNet.Interface = l.NetI
+	sNet.Vlan = l.LANTag
+	sNet.Network = randIP + ".0"
+	sNet.Min = randIP + ".6"
+	sNet.Max = randIP + ".254"
+	sNet.Router = randIP + ".1"
+	networks.Subnets = append(networks.Subnets, sNet)
+	ipList[sNet.Vlan] = randIP
 	f, err := ioutil.TempFile("", "dhcpd-conf")
 	if err != nil {
 		return nil, err
@@ -110,7 +114,7 @@ func New(ctx context.Context, ifaces map[string]string, bridge string, c *contro
 	}
 	cid := cont.ID()
 	for _, net := range networks.Subnets {
-		if err := addToSwitch(c, net, bridge, cid); err != nil {
+		if err := addToSwitch(c, net, l.Bridge, cid); err != nil {
 			log.Error().Msgf("Error on addToSwitch in dhcp %v ", err)
 		}
 	}
