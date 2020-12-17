73950c40a4b6ff7d9ef32f4fb0d4e9c118b37e1e
Ahmet Turkmen <f.ahmet.turkmen@icloud.com>
2020-12-17 20:57:59 +0300

    update service files for monitoring, gip, netman and wireguard
    Signed-off-by: Ahmet Turkmen <f.ahmet.turkmen@icloud.com>

--- scripts/packer/ubuntu-20.04/scripts/cleanup.sh
+++ scripts/packer/ubuntu-20.04/scripts/cleanup.sh
@@ -7,6 +7,8 @@ apt update
 # Delete unneeded files.
 rm -f /home/vagrant/*.sh
 rm -rf /home/vagrant/netman/netman_1.0.2_linux_64-bit.zip /home/vagrant/netman/netman_1.0.2_linux_64-bit
+rm -rf /home/vagrant/gwireguard*
+rm -rf /home/vagrant/gip_*.zip
 
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M
