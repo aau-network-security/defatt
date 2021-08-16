3e4fec818c78990893b9b7ca1fcd6db626d3acc9
Rasmi V. Mahmoud <rvm@es.aau.dk>
2021-08-16 13:55:19 +0200

    changing version of the gwireguard
    Signed-off-by: Rasmi V. Mahmoud <rvm@es.aau.dk>

--- scripts/packer/ubuntu-20.04/scripts/install_tools.sh
+++ scripts/packer/ubuntu-20.04/scripts/install_tools.sh
@@ -36,10 +36,10 @@ apt-get install git-all -y
 
 ## install wireguard gRPC service
 cd /home/vagrant
-wget https://github.com/aau-network-security/gwireguard/releases/download/v1.0.5/gwireguard_1.0.5_linux_64-bit.zip
-unzip gwireguard_1.0.5_linux_64-bit.zip && mv gwireguard_1.0.5_linux_64-bit/wgsservice /home/vagrant/wg-service
-chmod +x /home/vagrant/wg-service
-rm -rf gwireguard*
+wget https://github.com/aau-network-security/gwireguard/releases/download/v1.0.6/gwireguard_1.0.6_linux_64-bit.zip
+unzip gwireguard_1.0.6_linux_64-bit.zip && mv gwireguard_1.0.6_linux_64-bit/wgsservice /home/vagrant/gwireguard
+chmod +x /home/vagrant/gwireguard
+
 wget -P /home/vagrant/ https://raw.githubusercontent.com/aau-network-security/gwireguard/master/config/config.yml
 
 ## install gip gRPC service
