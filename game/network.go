1f0fa49eec49feb8f9558d927221a60ede4c45ac
Robert Nielsen <lanestolen@hotmail.dk>
2022-01-20 18:53:44 +0100

    remove networks when game is stopped

--- game/network.go
+++ game/network.go
@@ -38,9 +38,25 @@ func (env *environment) createPort(bridge string, input string, vlan int) error 
 	}
 
 	if err := env.controller.Ovs.VSwitch.AddPortTagged(bridge, name, fmt.Sprint(vlan)); err != nil {
-		log.Error().Err(err).Str("port", name).Msgf("adding port to switch")
+		log.Error().Err(err).Str("port", name).Msg("adding port to switch")
 		return err
 	}
 
+	env.ports = append(env.ports, name)
+
+	return nil
+}
+
+func (env *environment) removeNetworks(tag string) error {
+	for _, port := range env.ports {
+		if err := env.controller.IPService.LinkDel(port); err != nil {
+			log.Error().Err(err).Str("port", port).Msg("creating interface")
+			return err
+		}
+	}
+	if err := env.controller.Ovs.VSwitch.DeleteBridge(tag); err != nil {
+		log.Error().Err(err).Msg("deleting OVS bridge")
+		return err
+	}
 	return nil
 }
